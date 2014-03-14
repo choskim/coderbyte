afd40f06fcebeab40ffbdebb8b068a9af2575fb8
Cho S. Kim <4cho.kim@gmail.com>
2014-03-14 16:45:04 -0700

    adds spec and src files for countVowel.

---
+++ spec/countVowelSpec.js
@@ -0,0 +1,9 @@
+describe("countVowel", function() {
+  it("should count the total number of vowels in a string", function() {
+    expect(coderbyte.countVowel("hello")).toEqual(2);
+  });
+
+  it("should count the total number of vowels in a string", function() {
+    expect(coderbyte.countVowel("bookstore")).toEqual(4);
+  });
+})
---
+++ src/countVowel.js
@@ -0,0 +1,23 @@
+var coderbyte = coderbyte || {};
+
+// Version 1: A for-loop that has an artificial termination. 
+coderbyte.countVowel = function(string) {
+  var obj            = {},
+      stringOfVowels = 'aeiou',
+      count          = 0,
+      i,
+      j,
+      len;
+
+  for (i = 0, len = stringOfVowels.length; i < len; i++) {
+    obj[stringOfVowels[i]] = true;
+  }	
+
+  for (j = 0, len = string.length; j < len; j++) {
+  	if (obj[string[j]]) {
+  	  count++;
+  	}
+  }
+
+  return count;
+}

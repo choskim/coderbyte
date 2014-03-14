836409948fe1ca636aeb248f550d550e9fb911fa
Cho S. Kim <4cho.kim@gmail.com>
2014-03-14 14:25:10 -0700

    adds spec and src files for calculateFactorial.

---
+++ spec/calculateFactorialSpec.js
@@ -0,0 +1,5 @@
+describe("calculateFactorial", function() {
+  it("should calculate the factorial for an integer", function() {
+    expect(coderbyte.calculateFactorial(3)).toEqual(6);
+  });
+});
---
+++ src/calculateFactorial.js
@@ -0,0 +1,14 @@
+var coderbyte = coderbyte || {};
+
+// Version 1: A while-loop that has a natural termination. 
+coderbyte.calculateFactorial = function (integer) {
+  var product = 1;
+
+  while (integer) {
+  	product = product * integer; 
+    
+    integer--;
+  }
+
+  return product;
+}

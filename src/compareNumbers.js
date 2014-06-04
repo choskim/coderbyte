f12fcdd91d22428e7b32a508fcc70f3778baf730
William Wong <william.wong.nyc@gmail.com>
2014-06-04 14:18:12 -0700

    adds src files for compareNumbers.

---
+++ src/compareNumbers.js
@@ -0,0 +1,15 @@
+// Using the JavaScript language, have the function CheckNums(num1,num2) take both parameters being passed and return the string true if num2 is greater than num1, otherwise return the string false. If the parameter values are equal to each other then return the string -1. 
+
+function CheckNums(num1, num2) {
+  if (num1 < num2) {
+    answer = true;
+  } else {
+    answer = false;
+  }
+
+  if (num1==num2) {
+    answer = "-1";
+  }
+
+  return answer;
+}

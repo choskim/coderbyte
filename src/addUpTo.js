00bc04b968b902eccddc2e8ef7a7d913cb614d73
William Wong <william.wong.nyc@gmail.com>
2014-06-03 18:41:53 -0700

    adds src files for addUpTo.

---
+++ src/addUpTo.js
@@ -0,0 +1,15 @@
+// Using the JavaScript language, have the function SimpleAdding(num) add up all the numbers from 1 to num. For the test cases, the parameter num will be any number from 1 to 1000. 
+
+function addUpTo(num){
+  var total=0;
+  var i=1;
+  total=num;
+
+  while (i<=num) {
+    total+=i;
+    i++;
+  }
+
+  return total;
+}
+console.log(addUpTo(100));
